a97b034c414b56dabd3d9089c6f411aff4fbe83c
Hayden Larson <hlarson398@gmail.com>
2019-10-07 05:53:21 -0500

    added a app.listen for the server.js file

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
